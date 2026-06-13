2ba6626cd129beee91414375817d6420419d2c60
Harold <haroldsanabria13@gmail.com>
2026-06-13 18:21:56 -0500

    Proyecto EmpleateYa

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+console.log('ESTE ES MI SERVER.JS');
 const sql = require('mssql');
 const cors = require('cors');
 
@@ -31,6 +32,48 @@ app.get('/', (req, res) => {
     res.send('Servidor funcionando');
 });
 
+app.get('/usuarios', async (req, res) => {
+    try {
+        const result = await sql.query('SELECT * FROM usuario');
+        res.json(result.recordset);
+    } catch (err) {
+        console.error('ERROR SQL:', err);
+        res.status(500).send(err.message);
+    }
+});
+app.post('/registro', async (req, res) => {
+    try {
+        const {
+            nombre,
+            num_documento_identidad,
+            correo,
+            ciudad,
+            experiencia
+        } = req.body;
+
+        await sql.query`
+            INSERT INTO usuario
+            (nombre, num_documento_identidad, correo, ciudad, experiencia)
+            VALUES
+            (${nombre}, ${num_documento_identidad}, ${correo}, ${ciudad}, ${experiencia})
+        `;
+
+        res.send('Usuario registrado correctamente');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+app.get('/prueba-registro', async (req, res) => {
+    await sql.query`
+        INSERT INTO usuario
+        (nombre, num_documento_identidad, correo, ciudad, experiencia)
+        VALUES
+        ('Prueba Usuario', 123456, 'prueba@test.com', 'Zipaquirá', 'Sin experiencia')
+    `;
+
+    res.send('Registro insertado');
+});
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
 });
